2fc76ce42d781e387956272ee3a0d1966c290238
Caleb Matthew <caleb@crossware365.com>
2023-12-20 12:16:58 +1300

    another commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/webpushWebsite'),
+      dir: require('path').join(__dirname, './coverage/webpush'),
       subdir: '.',
       reporters: [
         { type: 'html' },
