ab2609bd6e57e77df1d6879401c19ec3c001021f
Caleb Matthew <caleb@crossware365.com>
2023-12-20 12:25:14 +1300

    should build hopefully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OneSignal } from 'onesignal-ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webpush';
-}
+  title = 'onesignal-website';
+
+  constructor(private oneSignal: OneSignal) {
+    // this.oneSignal.init({
+    //   appId: "47b373e4-cc58-4cce-a882-47dafae6f728",
+    // });
+  }
+
+  subFailed = this.oneSignal.Notifications.permissionNative;
+
+  isSupported = this.oneSignal.Notifications.isPushSupported();
+
+  subStatus = this.oneSignal.Notifications.permission;
+
+  addNotificationTag(key: string, tag: string) {
+    this.oneSignal.User.addTag(key, tag);
+  }
+
+  subToNotifications() {
+    this.oneSignal.Notifications.requestPermission();
+  }
+ }
